f9c17c4695a491f8981ea4ebb0136abc757a79f7
Tyler Stewart <tylers490@gmail.com>
2019-10-15 19:11:18 -0600

    feat: utilize hashes for parse history
    Currently, when you paste some json history isn't changed. So if you
    hit back, you'll accidentally discard the parsed json.
    
    Utilizing local storage and hashes, we can push a new hash to
    history in order to preserve our parses.
    
    This currently limited to 10 at a time so it doesn't grow too large.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const $instructions = document.getElementById('instructions');
 const $error = document.getElementById('error');
 
 const ERROR_SYMBOL = Symbol();
+const MAX_STORAGE_COUNT = 10;
 
 function formatJSON(json_string) {
   try {
@@ -33,11 +34,38 @@ $paste.onpaste = (event) => {
     $instructions.style.display = 'none';
     $error.style.display = 'block';
   } else {
-    $result.style.display = 'block';
-    $result.innerHTML = result;
-    $instructions.style.display = 'none';
-    $error.style.display = 'none';
+    const hash = Date.now();
+    window.localStorage.setItem(hash, result);
+    window.location.hash = hash;
+
+    // Delete the oldest key after we get more than `MAX_STORAGE_COUNT`
+    if (window.localStorage.length > MAX_STORAGE_COUNT) {
+      const keys = Object.keys(window.localStorage).sort();
+      const key = keys[0];
+      window.localStorage.removeItem(key);
+    }
   }
 
   return false; // prevent default
+};
+
+// Attempt to find the JSON object in storage by the url hash
+window.onhashchange = (event) => {
+  const hash = event.target.location.hash;
+  if (hash) {
+    const result = window.localStorage.getItem(hash.substring(1));
+    if (result) {
+      $result.style.display = 'block';
+      $result.innerHTML = result;
+      $instructions.style.display = 'none';
+      $error.style.display = 'none';
+      return true;
+    }
+  }
+
+  $result.style.display = 'none';
+  $result.innerHTML = '';
+  $instructions.style.display = 'block';
+  $error.style.display = 'none';
+  return true;
 };
